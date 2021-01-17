6c51a8d9bc8db589955fd075591889c0c7f24f82
mavicena21 <mavicena21@gmail.com>
2021-01-17 20:29:29 +0700

    add first program and str(), int(), and float() function

--- ep1.py
+++ ep1.py
@@ -7,3 +7,25 @@ print(x + y)
 print(x * y)
 print(y * z)
 print(spam)
+
+#string concatenation and replication
+aa = 'makan '
+bb = 'bang'
+print(aa + bb)
+print(aa * 5)
+
+#Assignment Statement
+spam = 42
+
+#My First Program
+print('Hello World!')
+print('What is your name? ')
+myname = input()
+print('Nice to meet you, ' + myname)
+print('The length of your name is:')
+print(len(myname))
+
+#str(), int(), and float() functions
+print(str(29))
+print(int(1.25))
+print(float(20))
